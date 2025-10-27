204fc1ac7191e3717da4e3e8fb07df64ced3fc13
usakochan <ryokun8500@keio.jp>
2025-10-27 15:57:26 +0900

    javaで対処してみた。よくわからん。

--- Shape_Java.java
+++ Shape_Java.java
@@ -22,12 +22,25 @@ class Circle extends Ellipse{ //クラスの定義
 }
 //データ構造と関連する関数を定めるクラスの定義
 
-void run(){
-    Rectangle r = new Rectangle (5.0, 8.0); //rがRectangle型のデータを保持することを宣言している
-    Ellipse   u = new Ellipse (3.0, 4.0);   //uがEllipse型のデータを保持することを宣言している
-    Circle    v = new Circle (3.0);         //vがCircle型のデータを保持することを宣言している
-    //↑定義ブロック
-    Shape [] ss = new Shape[]{r, u, v}; //配列ssに格納。Shapeを継承し続けたことで、Shape型の配列として定義が可能。
-    for (Shape s : ss) { putStr("area: " + s.area() + "\n");} //"\n"はリテラル
-    //↑使用ブロック
-} 
+// void run(){
+//     Rectangle r = new Rectangle (5.0, 8.0); //rがRectangle型のデータを保持することを宣言している
+//     Ellipse   u = new Ellipse (3.0, 4.0);   //uがEllipse型のデータを保持することを宣言している
+//     Circle    v = new Circle (3.0);         //vがCircle型のデータを保持することを宣言している
+//     //↑定義ブロック
+//     Shape [] ss = new Shape[]{r, u, v}; //配列ssに格納。Shapeを継承し続けたことで、Shape型の配列として定義が可能。
+//     for (Shape s : ss) { putStr("area: " + s.area() + "\n");} //"\n"はリテラル
+//     //↑使用ブロック
+// } 
+
+public class Shape_Java{
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(5.0, 8.0);
+        Ellipse u = new Ellipse(3.0, 4.0);
+        Circle v = new Circle(3.0);
+
+        Shape[] ss = new Shape[]{r, u, v};
+        for (Shape s : ss) {
+            System.out.println("area: " + s.area());
+        }
+    }
+}
